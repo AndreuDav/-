a06ca13230da4a159ccddaec34400a44bcf4900d
Кирилл <115004846+zxcsunrise@users.noreply.github.com>
2024-10-29 10:12:21 +0400

    first

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-to_do_list = []
+to_do_to_do_list = []
 question = []
-while question != 4:
-    question = int(input('Введите номер действия которое хотите сделать'
-      '1 - добавить задачу, 2 - просмотреть список всех задач'
-      '3 - удалить задачу, 4 выйти из программы: '))
-    if question == 1:
-        to_do_list.append(input('Введите текст задачи по числам: '))
+
+def add_task():
+    to_do_list.append(input('Введите текст задачи: '))
+    print(to_do_list)
+
+def view_tasks():
         print(to_do_list)
-    if question == 2:
-        print(to_do_list)
-    if question == 3:
-        print(to_do_list)
-        to_do_list.pop(int(input('Введите индекс задачи которую хотите удалить')))
+
+def delete_task():
+    print(to_do_list)
+    to_do_list.pop(int(input('Введите индекс задачи которую хотите удалить')))
+
+
+
+def main():
+    while True:
+        question = int(input('Введите номер действия которое хотите сделать'
+                             ' 1 - добавить задачу, 2 - просмотреть список всех задач'
+                             ' 3 - удалить задачу, 4 выйти из программы: '))
+
+        if question == 1:
+            add_task()
+        elif question == 2:
+            view_tasks()
+        elif question == 3:
+            delete_task()
+        else:
+            break
+main()
--- test.py
+++ test.py
@@ -1,19 +1,31 @@
 to_do_list = []
 question = []
-while True:
-    main = int(input('Введите номер действия которое хотите сделать'
-      '1 - добавить задачу, 2 - просмотреть список всех задач'
-      '3 - удалить задачу, 4 выйти из программы: '))
-    if main == 1:
-        add_task = to_do_list.append(input('Введите текст задачи по числам: '))
+
+def add_task():
+    to_do_list.append(input('Введите текст задачи: '))
+    print(to_do_list)
+
+def view_tasks():
         print(to_do_list)
-    elif main == 2:
-        view_tasks = print(to_do_list)
-    elif main == 3:
-        print(to_do_list)
-        delete_task = to_do_list.pop(int(input('Введите индекс задачи которую хотите удалить')))
-    elif main == 4:
-        print('Программа завершена')
-        break
-    else:
-        print('Ошибка, введите корректное значение!')
+
+def delete_task():
+    print(to_do_list)
+    to_do_list.pop(int(input('Введите индекс задачи которую хотите удалить')))
+
+
+
+def main():
+    while True:
+        question = int(input('Введите номер действия которое хотите сделать'
+                             ' 1 - добавить задачу, 2 - просмотреть список всех задач'
+                             ' 3 - удалить задачу, 4 выйти из программы: '))
+
+        if question == 1:
+            add_task()
+        elif question == 2:
+            view_tasks()
+        elif question == 3:
+            delete_task()
+        else:
+            break
+main()
